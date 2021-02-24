5cb6a923806776b8b002cbd55612ea4572790549
PresentKim <debe3721@gmail.com>
2021-02-24 16:23:44 +0900

    Set license to LGPL and add PHP DOC

--- src/kim/present/plantsscanner/Loader.php
+++ src/kim/present/plantsscanner/Loader.php
@@ -1,4 +1,30 @@
 <?php
+
+/**
+ *  ____                           _   _  ___
+ * |  _ \ _ __ ___  ___  ___ _ __ | |_| |/ (_)_ __ ___
+ * | |_) | '__/ _ \/ __|/ _ \ '_ \| __| ' /| | '_ ` _ \
+ * |  __/| | |  __/\__ \  __/ | | | |_| . \| | | | | | |
+ * |_|   |_|  \___||___/\___|_| |_|\__|_|\_\_|_| |_| |_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author  PresentKim (debe3721@gmail.com)
+ * @link    https://github.com/PresentKim
+ * @license https://www.gnu.org/licenses/lgpl-3.0 LGPL-3.0 License
+ *
+ *   (\ /)
+ *  ( . .) ♥
+ *  c(")(")
+ *
+ * @noinspection PhpIllegalPsrClassPathInspection
+ * @noinspection SpellCheckingInspection
+ * @noinspection PhpDocSignatureInspection
+ */
+
 declare(strict_types=1);
 
 namespace kim\present\plantsscanner;
@@ -16,6 +42,8 @@ use pocketmine\item\ItemIds;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\SingletonTrait;
+
+use function max;
 
 final class Loader extends PluginBase implements Listener{
     use SingletonTrait;
